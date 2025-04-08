c22a505917cda9097c5ddd56a4c6d42157553b11
flowtricks <100830477+flowtricks@users.noreply.github.com>
2025-04-07 20:18:28 -0500

    Update match-container.js

--- match-container.js
+++ match-container.js
@@ -19,4 +19,111 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. */
-(()=>{const e="bae45330cd3d4e0e96b60d26b57009b5",t=Symbol.for(e);window[t]=window[t]??(()=>{let t=0;return{createID:()=>`${e}-${Date.now()}-${t++}`}})();const r=window[t].createID;class n extends Event{container;matches;constructor(e){super(e)}}class a extends EventTarget{container;matches;constructor(e,t){super(),this.container=t;const n="container-query-observer-"+r(),a=`data-${n}`;e.setAttribute(a,"");const o=`--${n}`,i=new CSSStyleSheet,s=`@property ${o} {\n          syntax: '<custom-ident>';\n          inherits: false;\n          initial-value: --false;\n        }\n        @container ${t} { [${a}] { ${o}: --true; } }`;i.replaceSync(s),document.adoptedStyleSheets=[...document.adoptedStyleSheets,i];const c=getComputedStyle(e);this.matches=c.getPropertyValue(o)==="--true",this.#e(o,t,e)}#e(e,t,r){const a=n=>{if(e in n){const r=n[e]==="--true";this.matches=r;const a=new n("change");a.matches=r,a.container=t,this.dispatchEvent(a)}};const o={},i=getComputedStyle(r).getPropertyValue(e);o[e]=i,r.style.setProperty("transition",`${e} 0.001ms step-start`),r.style.setProperty("transition-behavior","allow-discrete"),r.addEventListener("transitionrun",n=>{if(r===n.target){const t=getComputedStyle(n.target),i=t.getPropertyValue(e),s=o[e];i!==s&&(o[e]=i,a({[e]:i}))}})}}Element.prototype.matchContainer||(Element.prototype.matchContainer=function(e){return new a(this,e)})})();
+(() => {  
+    const polyfill_key = "bae45330cd3d4e0e96b60d26b57009b5";
+    const polyfill = Symbol.for(polyfill_key);
+    window[polyfill] =
+      window[polyfill] ??
+      (() => {
+        let count = 0;
+        return { createID: () => `${polyfill_key}-${Date.now()}-${count++}` };
+      })();
+    const createID = window[polyfill].createID;
+  
+    class ContainerQueryListEvent extends Event {
+      container;
+  
+      matches;
+  
+      constructor(type) {
+        super(type);
+      }
+    }
+    class ContainerQueryList extends EventTarget {
+      container;
+  
+      matches;
+  
+      constructor(element, containerQueryString) {
+        super();
+        this.container = containerQueryString;
+        const unique_name = "container-query-observer-" + createID();
+        const markerAttribute = `data-${unique_name}`;
+        element.setAttribute(markerAttribute, "");
+        const sentinelProperty = `--${unique_name}`;
+        const containerQuerySheet = new CSSStyleSheet();
+        const css = `
+        @property ${sentinelProperty} {
+          syntax: '<custom-ident>';
+          inherits: false;
+          initial-value: --false;
+        }
+        @container ${containerQueryString} { [${markerAttribute}] { ${sentinelProperty}: --true; } }`;
+  
+        containerQuerySheet.replaceSync(css);
+        document.adoptedStyleSheets = [
+          ...document.adoptedStyleSheets,
+          containerQuerySheet,
+        ];
+        const style = getComputedStyle(element);
+        this.matches = style.getPropertyValue(sentinelProperty) === "--true";
+  
+        this.#startObserving(sentinelProperty, containerQueryString, element);
+      }
+  
+      #startObserving(sentinelProperty, containerQueryString, observedElement) {
+        const _callback = (values) => {
+          if (sentinelProperty in values) {
+            const matches = values[sentinelProperty] === "--true";
+            this.matches = matches;
+            // raise 'change' event
+            const event = new ContainerQueryListEvent("change");
+            event.matches = matches;
+            event.container = containerQueryString;
+            this.dispatchEvent(event);
+          }
+        };
+  
+        const _previousValues = {};
+  
+        observedElement.style.setProperty(
+          "transition",
+          `${sentinelProperty} 0.001ms step-start`
+        );
+        observedElement.style.setProperty(
+          "transition-behavior",
+          "allow-discrete"
+        );
+        const onTransitionRun = (e) => {
+          const targetElement = e.target;
+  
+          if (observedElement === targetElement) {
+            const computedStyle = getComputedStyle(targetElement);
+            const changes = {};
+            const currentValue = computedStyle.getPropertyValue(sentinelProperty);
+            const previousValue = _previousValues[sentinelProperty];
+            const hasChanged = currentValue !== previousValue;
+  
+            if (hasChanged) {
+              changes[sentinelProperty] = currentValue;
+              _previousValues[sentinelProperty] = currentValue;
+              _callback(changes);
+            }
+          }
+        };
+        observedElement.addEventListener("transitionrun", onTransitionRun);
+  
+        // init _previousValues
+        const computedStyle = getComputedStyle(observedElement);
+        const currentValue = computedStyle.getPropertyValue(sentinelProperty);
+        _previousValues[sentinelProperty] = currentValue;
+      }
+    }
+  
+    if (Element.prototype.matchContainer) return;
+  
+    function matchContainer(containerQueryString) {
+      return new ContainerQueryList(this, containerQueryString);
+    }
+    Element.prototype.matchContainer = matchContainer;
+  })();
